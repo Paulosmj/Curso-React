575cd5bc2d3a63ed5a824827aad4dc44cf358474
Paulosmj <psmeneguesso@gmail.com>
2022-01-07 16:16:02 -0300

    React fragment - tag

---
+++ src/components/basicos/Fragmento.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Fragmento(props) {
+  return (
+    // fragment, ou também pode usar React.Fragment no lugar da tag
+    <>
+      <h2>Fragmento</h2>
+      <p>Cuidado com esse erro!</p>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
 
 ReactDOM.render(
   <div id="app">
     <Primeiro />
     <ComParametro titulo="Situação do aluno" aluno="Pedro Silva" nota={9.3} />
+    <Fragmento />
   </div>,
   document.getElementById('root'),
 );
