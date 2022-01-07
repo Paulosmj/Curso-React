d07e864d85579d951b04413a975e9dfbe010bb00
Paulosmj <psmeneguesso@gmail.com>
2022-01-07 17:13:28 -0300

    Desafio numero aleatorio com component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable */
 import React from 'react';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
+import NumeroAleatorio from './components/basicos/NumeroAleatorio';
 
 export default () => (
   <div id="app">
     <h1>Fundamentos React</h1>
+    <NumeroAleatorio min={1} max={60} />
     <Fragmento />
 
     <ComParametro titulo="Situação do aluno" aluno="Pedro Silva" nota={9.3} />
