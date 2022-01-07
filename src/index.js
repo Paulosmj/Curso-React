94f154236d282ca0406d1386103ccc38a6687f2a
Paulosmj <psmeneguesso@gmail.com>
2022-01-07 16:34:44 -0300

    Componente app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import './index.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Primeiro from './components/basicos/Primeiro';
-import ComParametro from './components/basicos/ComParametro';
-import Fragmento from './components/basicos/Fragmento';
+import App from './App';
 
-ReactDOM.render(
-  <div id="app">
-    <Primeiro />
-    <ComParametro titulo="Situação do aluno" aluno="Pedro Silva" nota={9.3} />
-    <Fragmento />
-  </div>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
