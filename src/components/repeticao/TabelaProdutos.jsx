22fcdbf75065197e7d2cb4b779f7f74ef6c05152
Paulosmj <psmeneguesso@gmail.com>
2022-01-11 15:33:55 -0300

    Refazendo desafio com renderizacao condicional

--- src/components/repeticao/TabelaProdutos.jsx
+++ src/components/repeticao/TabelaProdutos.jsx
@@ -1,20 +1,34 @@
 /* eslint-disable */
 
+import './TabelaProdutos.css';
 import React from 'react';
 import produtos from '../../data/produtos';
 
 export default (props) => {
-  const produtosLi = produtos.map((produto) => {
-    return (
-      <li key={produto.id}>
-        {produto.id}) {produto.nome} - {produto.preco}
-      </li>
-    );
-  });
+  function getLinhas() {
+    return produtos.map((produto, i) => {
+      return (
+        <tr key={produto.id} className={i % 2 == 0 ? 'Par' : 'Impar'}>
+          <td>{produto.id}</td>
+          <td>{produto.nome}</td>
+          <td>R$ {produto.preco.toFixed(2).replace('.', ',')}</td>
+        </tr>
+      );
+    });
+  }
 
   return (
-    <div>
-      <ul style={{ listStyle: 'none' }}>{produtosLi}</ul>
+    <div className="TabelaProdutos">
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Preço</th>
+          </tr>
+        </thead>
+        <tbody>{getLinhas()}</tbody>
+      </table>
     </div>
   );
 };
--- src/data/produtos.js
+++ src/data/produtos.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 
 export default [
-  { id: 1, nome: 'caderno', preco: 'R$ 8.50' },
-  { id: 2, nome: 'lapis', preco: 'R$ 2.50' },
-  { id: 3, nome: 'caneta', preco: 'R$ 3.50' },
-  { id: 4, nome: 'borracha', preco: 'R$ 1.50' },
-  { id: 5, nome: 'clips', preco: 'R$ 0.25' },
-  { id: 6, nome: 'livro', preco: 'R$ 15.00' },
-  { id: 7, nome: 'estojo', preco: 'R$ 5.50' },
-  { id: 8, nome: 'apontador', preco: 'R$ 2.00' },
-  { id: 9, nome: 'corretivo', preco: 'R$ 3.00' },
+  { id: 1, nome: 'caderno', preco: 8.5 },
+  { id: 2, nome: 'lapis', preco: 2.5 },
+  { id: 3, nome: 'caneta', preco: 3.5 },
+  { id: 4, nome: 'borracha', preco: 1.5 },
+  { id: 5, nome: 'clips', preco: 0.25 },
+  { id: 6, nome: 'livro', preco: 15.0 },
+  { id: 7, nome: 'estojo', preco: 5.5 },
+  { id: 8, nome: 'apontador', preco: 2.0 },
+  { id: 9, nome: 'corretivo', preco: 3.0 },
 ];
