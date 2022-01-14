a2d2d7dbd304b7331fbfb11944f6ad3e9f3b3676
Paulosmj <psmeneguesso@gmail.com>
2022-01-14 17:41:58 -0300

    Contador #1

---
+++ src/components/contador/Contador.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable */
+
+import './contador.css';
+import React, { Component } from 'react';
+
+export default class Contador extends Component {
+  state = {
+    numero: this.props.numeroInicial,
+    passo: this.props.passoInicial || 5,
+  };
+
+  inc = () => {
+    this.setState({
+      numero: this.state.numero + this.state.passo,
+    });
+  };
+
+  dec = () => {
+    this.setState({
+      numero: this.state.numero - this.state.passo,
+    });
+  };
+
+  setPasso = (event) => {
+    this.setState({
+      passo: +event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="Contador">
+        <h2>Contador</h2>
+        <h3>{this.state.numero}</h3>
+        <div>
+          <label htmlFor="passoInput">Passo: </label>
+          <input
+            id="passoInput"
+            type="number"
+            value={this.state.passo}
+            onChange={this.setPasso}
+          />
+        </div>
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+      </div>
+    );
+  }
+}
