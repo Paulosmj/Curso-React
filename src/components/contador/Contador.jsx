75395b3d7e92d9d6618f2d74cf9e85a5ec1ffa5a
Paulosmj <psmeneguesso@gmail.com>
2022-01-14 18:01:30 -0300

    Contador #2

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -2,6 +2,9 @@
 
 import './contador.css';
 import React, { Component } from 'react';
+import Display from './Display';
+import Botoes from './Botoes';
+import PassoForm from './PassoForm';
 
 export default class Contador extends Component {
   state = {
@@ -21,9 +24,9 @@ export default class Contador extends Component {
     });
   };
 
-  setPasso = (event) => {
+  setPasso = (novoPasso) => {
     this.setState({
-      passo: +event.target.value,
+      passo: novoPasso,
     });
   };
 
@@ -31,18 +34,9 @@ export default class Contador extends Component {
     return (
       <div className="Contador">
         <h2>Contador</h2>
-        <h3>{this.state.numero}</h3>
-        <div>
-          <label htmlFor="passoInput">Passo: </label>
-          <input
-            id="passoInput"
-            type="number"
-            value={this.state.passo}
-            onChange={this.setPasso}
-          />
-        </div>
-        <button onClick={this.inc}>+</button>
-        <button onClick={this.dec}>-</button>
+        <Display numero={this.state.numero} />
+        <PassoForm passo={this.state.passo} setPasso={this.setPasso} />
+        <Botoes setInc={this.inc} setDec={this.dec} />
       </div>
     );
   }
---
+++ src/components/contador/Display.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable */
+
+import React from 'react';
+
+export default (props) => {
+  return <h3>{props.numero}</h3>;
+};
